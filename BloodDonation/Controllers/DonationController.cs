a0379fd46ddd2bc5bff735a94b661e64e94f1819
MrThalamus <python373737@gmail.com>
2026-07-25 20:53:30 +0600

    Donation CRUD

---
+++ BloodDonation/Controllers/DonationController.cs
@@ -0,0 +1,109 @@
+﻿using BloodDonation.EF;
+using BloodDonation.EF.Tables;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloodDonation.Controllers
+{
+    public class DonationController : Controller
+    {
+        BloodBankDbContext dc;
+        public DonationController(BloodBankDbContext dc)
+        {
+            this.dc = dc;
+        }
+
+        public IActionResult Read()
+        {
+            var donations = dc.Donations.ToList();
+            return View(donations);
+        }
+        [HttpGet]
+        public IActionResult Create() //=======================================
+        {
+            ViewBag.Donors = dc.Donors.ToList();
+
+            var donation = new Donation
+            {
+                DonationDate = DateOnly.FromDateTime(DateTime.Today)
+            };
+
+            return View(donation);
+        }
+        [HttpPost]
+        public IActionResult Create(Donation donation)
+        {
+            var donorExists = dc.Donors.Any(d => d.DonorId == donation.DonorId);
+
+            if (!donorExists)
+            {
+                ModelState.AddModelError("DonorId", "Donor does not exist.");
+            }
+
+            if (donation.DonationDate > DateOnly.FromDateTime(DateTime.Today))
+            {
+                ModelState.AddModelError("DonationDate",
+                    "Donation date cannot be in the future.");
+            }
+            
+            if (ModelState.IsValid)
+            {
+                dc.Donations.Add(donation);
+                dc.SaveChanges();
+                return RedirectToAction("Read");
+            }
+            ViewBag.Donors = dc.Donors.ToList();
+            return View(donation);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id) //========================================
+        {
+            ViewBag.Donors = dc.Donors.ToList();
+            var info = dc.Donations.Find(id);
+            return View(info);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, Donation updatedDonation)
+        {
+            var existingDonation = dc.Donations.Find(id);
+            if (existingDonation == null)
+            {
+                return NotFound();
+            }
+
+            existingDonation.DonorId = updatedDonation.DonorId;
+            existingDonation.DonationDate = updatedDonation.DonationDate;
+            existingDonation.VolumeMl = updatedDonation.VolumeMl;
+            existingDonation.CampName = updatedDonation.CampName;
+
+            dc.SaveChanges();
+            ViewBag.Donors = dc.Donors.ToList();
+            return RedirectToAction("Read");
+        }
+        [HttpGet]
+        public IActionResult Delete(int id) //========================================
+        {
+            ViewBag.Donors = dc.Donors.ToList();
+            var info = dc.Donations.Find(id);
+            if (info == null)
+            {
+                return NotFound();
+            }
+
+            return View(info);
+        }
+        [HttpPost]
+        public IActionResult Delete(int id, Donation donation)
+        {
+            ViewBag.Donors = dc.Donors.ToList();
+            var info = dc.Donations.Find(id);
+            if (info == null)
+            {
+                return NotFound();
+            }
+
+            dc.Donations.Remove(info);
+            dc.SaveChanges();
+            return RedirectToAction("Read");
+        }
+    }
+}
--- BloodDonation/EF/Tables/Donation.cs
+++ BloodDonation/EF/Tables/Donation.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BloodDonation.EF.Tables;
 
@@ -7,13 +9,20 @@ public partial class Donation
 {
     public int DonationId { get; set; }
 
+    [Required]
     public int DonorId { get; set; }
 
+    [Required(ErrorMessage = "Donation Date is required")]
     public DateOnly DonationDate { get; set; }
 
+    [Required(ErrorMessage = "Volume in ML is required")]
+    [Range(250, 500, ErrorMessage = "Volume must be between 250 and 500 mL.")]
     public int VolumeMl { get; set; }
 
+    [Required(ErrorMessage = "Camp Name is required")]
+    [StringLength(50, ErrorMessage = "Camp Name cannot exceed 50 characters.")]
     public string CampName { get; set; } = null!;
 
+    [ValidateNever]
     public virtual Donor Donor { get; set; } = null!;
 }
